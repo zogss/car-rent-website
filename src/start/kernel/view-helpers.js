6fab0bb9f8848b50f67cf0e2fba434a763e6e0f6
Manuel-Antunes <nerdbroz2@gmail.com>
2022-07-21 22:45:24 -0300

    feat: add create hash function

--- src/start/kernel/view-helpers.js
+++ src/start/kernel/view-helpers.js
@@ -1,5 +1,10 @@
+import crypto from 'crypto';
+
 export default {
-    json(val) {
-      return JSON.stringify(val, null, 2);
-    },
-  };
+  json(val) {
+    return JSON.stringify(val, null, 2);
+  },
+  md5(str) {
+    return crypto.createHash("md5").update(str).digest("hex");
+  },
+};
